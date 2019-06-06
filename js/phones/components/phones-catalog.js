74b5566042dd7dc5983f419315065581b7244ba3
Ivan Kuzmenko <deoiad@gmail.com>
2019-06-06 19:19:57 +0300

    subscribe/emit almost implemented

--- js/phones/components/phones-catalog.js
+++ js/phones/components/phones-catalog.js
@@ -4,32 +4,16 @@ export default class PhonesCatalog extends Component {
     constructor({
         element,
         phones = []
-        // onPhoneSelected = () => {}
     }) {
         super({ element });
-        this._callbackMap = {};
         this._phones = phones;
-        // this.onPhoneSelected = onPhoneSelected;
         this._render();
 
         this.on('click', '[data-element="details-link"]', (event) => {
             const phoneEl = event.target.closest('[data-element="phone-element"]');
             const phoneId = phoneEl.dataset.phoneId;
-            // this.onPhoneSelected(phoneId);
             this.emit('phone-selected', phoneId);
         })
-    }
-
-    emit(eventName, data) {
-        const callback = this._callbackMap[eventName];
-        if (!callback) {
-            return;
-        }
-        callback(data);
-    }
-
-    subscribe(eventName, callback) {
-        this._callbackMap[eventName] = callback;
     }
 
     _render() {
